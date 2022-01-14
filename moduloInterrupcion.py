883a9d1ed6cef6020e9dfe963bc558c68e5d1c81
ailr16 <angel_ivan_5@outlook.com>
2022-01-14 17:17:34 -0600

    Script funcionando dirigido por eventos (interrupcion en GPIO23)

--- moduloInterrupcion.py
+++ moduloInterrupcion.py
@@ -14,6 +14,7 @@ uart = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout = 1)
 
 CONNECTION_STRING = "HostName=monitoreoPolucion.azure-devices.net;DeviceId=m1;SharedAccessKey=oH7JV1wKJ8QDnxJQFLJU5c8tlEDuY/Sln2bVXH8YqAU=" 
 MSG_SND = '{{"co2", {co2},"pm25", {pm25},"pm10", {pm10},"temp", {temp}}}'
+client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
 def leeModulo():
     lista = []
@@ -43,42 +44,31 @@ def leeModulo():
     
     return [co2, pm25, pm10, temp]
 
+def iothub_client_telemetry_sample_run(client):
+    print ( "Sending data to IoT Hub, press Ctrl-C to exit" )
+    sensores = leeModulo()
+    msg_txt_formatted = MSG_SND.format(co2=sensores[0], pm25=sensores[1], pm10=sensores[2], temp=sensores[3])
+    message = Message(msg_txt_formatted)
+    print( "Sending message: {}".format(message) )
+    client.send_message(message)
+    print ( "Message successfully sent" )
+
+def i_event(channel):
+        global i
+        global client
+        i = i + 1
+        print("Flanco detectado" + str(i))
+        global sensores
+        if i == 20:
+            #sensores = leeModulo()
+            iothub_client_telemetry_sample_run(client)
+            i = 0
+        else:
+            sensores = sensores
+
 GPIO.setup(in_signal, GPIO.IN)
 GPIO.add_event_detect(in_signal, GPIO.FALLING, callback=i_event)
 
 while True:
-    def i_event(channel):
-        global i
-        i = i + 1
-        print("Flanco detectado" + str(i))
-        global sensores
-        global stat
-        if i == 20:
-            sensores = leeModulo()
-            stat = 1
-            iothub_client_telemetry_sample_run()
-            i = 0
-        else:
-            sensores = sensores
-            stat = 0
+    pass
     
-    def iothub_client_init():  
-        client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)  
-        return client
-    
-    def iothub_client_telemetry_sample_run():
-        try:
-            client = iothub_client_init()
-            print ( "Sending data to IoT Hub, press Ctrl-C to exit" )
-            global sensores
-            msg_txt_formatted = MSG_SND.format(co2=sensores[0], pm25=sensores[1], pm10=sensores[2], temp=sensores[3])
-            message = Message(msg_txt_formatted)
-            print( "Sending message: {}".format(message) )
-            client.send_message(message)
-            print ( "Message successfully sent" )
-            
-        except KeyboardInterrupt:
-                print ( "IoTHubClient stopped" )  
-    #if __name__ == '__main__':  
-     #   print ( "Press Ctrl-C to exit" )  
-      #  iothub_client_telemetry_sample_run()
