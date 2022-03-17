2b2e8c07cc88e1a0d99189353a6bf3bdfc4fd7ca
ailr16 <angel_ivan_5@outlook.com>
2022-03-17 16:31:28 -0600

    Actualizacion valor inicial del contador de eventos

--- scripts/scriptFinal.py
+++ scripts/scriptFinal.py
@@ -12,7 +12,7 @@ import subprocess                   #Procesos del SO
 
 ##########################Variables generales###########################
 GPIO.setmode(GPIO.BOARD)            #Numeracion de la tarjeta
-i = 13                               #Variable para contador
+i = 12                               #Variable para contador
 wifi_status = False
 med_sonido = [0,0,0,0]
 
